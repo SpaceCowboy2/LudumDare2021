b464b353e2af029469475905c88e2de49dab9777
Theo Driutti <theo.driutti57@gmail.com>
2021-10-02 14:28:00 +0200

    Script Tangage

---
+++ Lududududum2021/Assets/Tangage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tangage : MonoBehaviour
+{
+    public float maxAngle;
+    public float angularSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = Mathf.PingPong(Time.time, 2 * angularSpeed) - angularSpeed;
+        transform.rotation = Quaternion.Euler(0, 0, maxAngle * angle);
+    }
+}
